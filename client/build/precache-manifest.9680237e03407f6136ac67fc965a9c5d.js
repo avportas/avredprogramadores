cd95ca1f0aff781075e711b7c4aac0bde246bf68
Al Tfe <heroku7@gmx.es>
2020-01-20 18:37:15 +0000

    primer commit

---
+++ client/build/precache-manifest.9680237e03407f6136ac67fc965a9c5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989217d25d7076277aa934a0c4b375d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7806ae8daa97dcf2cd1",
+    "url": "/static/css/main.11b62bc3.chunk.css"
+  },
+  {
+    "revision": "36c68d140a8553eebb51",
+    "url": "/static/js/2.f738b9b9.chunk.js"
+  },
+  {
+    "revision": "e7806ae8daa97dcf2cd1",
+    "url": "/static/js/main.768af2f7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
+
+const app = express();
+
+// Connect Database
+//connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+
+var mongoose = require('mongoose'); //--> declaramos dependencia con mongoose
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/devconnector2', {
+    useNewUrlParser: true
+});
+
+
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
